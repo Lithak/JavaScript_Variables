6d493e11c5749c9b79ce921382b65dc0a1a3be34
Litha Kane <71072996+Lithak@users.noreply.github.com>
2020-11-03 15:55:11 +0200

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,18 @@
+let Name = "Litha";
+console.log(Name);
+let Surname = "Kane";
+console.log(Surname);
+console.log(Name+Surname);
+document.write("<br/><br/><br/>");
+
+let numberVar = 5;
+console.log(numberVar);
+
+document.write("<br/><br/><br/>");
+document.write("4. The output when I type numberVar * 5 is : 25");
+document.write("<br/><br/><br/>");
+
+const myname = "Litha";
+console.log(myname);
+document.write("<br/><br/><br/>");
+document.write("5. Yes I got An error trying to re write a const");
